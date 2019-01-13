91cfb6edb10b8fe13c39b99c4a155ec7638c6d2f
Daian <daian.aiziatov@gmail.com>
2019-01-13 18:08:07 -0500

    Trying to synchronize scanning and data task

--- RemoteControlPhilips/LANScanner.swift
+++ RemoteControlPhilips/LANScanner.swift
@@ -12,6 +12,7 @@ class LANScanner: NSObject {
     
     @objc dynamic var isScanRunning : BooleanLiteralType = false
     private var myContext = 0
+    private var connectedDevices = [MMDevice]()
     
     private var client: APIClient!
     private var tvInfo: SystemInfo?
@@ -33,11 +34,26 @@ class LANScanner: NSObject {
         completion(Result.success(tvInfo))
     }
     
-    private func tryHost(with ipAddress: String) {
+    private func tryConnectedDevices() {
+        for index in 0..<connectedDevices.count {
+            if index == (connectedDevices.count - 1) {
+                tryHost(with: connectedDevices[index].ipAddress, isLast: true)
+            } else {
+                tryHost(with: connectedDevices[index].ipAddress, isLast: false)
+            }
+        }
+    }
+    
+    private func tryHost(with ipAddress: String, isLast: Bool) {
         client = APIClient(ipAddress: ipAddress)
         client.fetchingSystemInfo() { result in
             switch result {
-            case .failure(let error): print(error.reason)
+            case .failure(let error):
+                print(error.reason)
+                if isLast {
+                    self.isScanRunning = false
+                    print("Process is complited")
+                }
             case .success(let response):
                 self.tvInfo = response
                 print("SUCCESS: \(response.ipAddress!)")
@@ -53,17 +69,20 @@ class LANScanner: NSObject {
 extension LANScanner: MMLANScannerDelegate {
     
     func lanScanDidFindNewDevice(_ device: MMDevice!) {
-        tryHost(with: device.ipAddress)
+        if(!self.connectedDevices.contains(device)) {
+            self.connectedDevices.append(device)
+        }
+
     }
     
     func lanScanDidFinishScanning(with status: MMLanScannerStatus) {
-        self.isScanRunning = false
-        print("SCAN finished")
+        print("SCAN finished, TryingHosts Started")
+        self.tryConnectedDevices()
     }
     
     func lanScanDidFailedToScan() {
         self.isScanRunning = false
-         print("SCAN failed")
+        print("SCAN failed")
     }
     
     
